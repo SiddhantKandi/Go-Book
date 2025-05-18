c046cd0733e567adb404acc2897c103ec7245255
Siddhant kandi <kandisiddhant@gmail.com>
2025-05-18 15:36:50 +0530

    Initial Project

---
+++ Chapter1 Variables and Operators/Activity1.01/main.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"	
+)
+
+func main() {
+	firstName := "Bob"
+	lastName := "Smith"
+	age := 34
+	peanutAllergy := false
+
+	fmt.Println("First Name:", firstName)
+	fmt.Println("Last Name:", lastName)
+	fmt.Println("Age:", age)
+	fmt.Println("Penut Allergy : ",peanutAllergy)
+    
+}
